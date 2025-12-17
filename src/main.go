6fb15a140b1ff4c7cc507bdde044f825db352869
Anshuman Khanna <anshumankh01@gmail.com>
2025-12-17 11:57:46 +0530

    Seperated JSON extraction into seperate files

--- src/main.go
+++ src/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
 	"log"
@@ -9,37 +8,7 @@ import (
 	"os/exec"
 )
 
-type VarsType struct {
-	Browsers  []string `json:"browsers"`
-	Terminals []string `json:"terminals"`
-	Dirs      []struct {
-		Alias string `json:"alias"`
-		Dir   string `json:"dir"`
-	}
-}
-
-type AppDataType struct {
-	App  string    `json:"app"`
-	Args *[]string `json:"args"`
-}
-
-type CommandDataType struct {
-	Url     *string        `json:"url"`
-	Dir     *string        `json:"dir"`
-	AppData *[]AppDataType `json:"appData"`
-}
-
-type AliasObjectType struct {
-	Alias string `json:"alias"`
-	CommandDataType
-}
-
 type AliasMap map[string]CommandDataType
-
-type JsonDataType struct {
-	Vars    VarsType          `json:"vars"`
-	Aliases []AliasObjectType `json:"aliases"`
-}
 
 func (aliases AliasMap) Run(alias string) (*exec.Cmd, error) {
 	commandData, ok := aliases[alias]
@@ -54,44 +23,7 @@ func (aliases AliasMap) Run(alias string) (*exec.Cmd, error) {
 }
 
 func main() {
-	const filepath = "commands.json"
-
-	jsonBytes, err := os.ReadFile(filepath)
-	if err != nil {
-		log.Fatalf("Error reading JSON file %s: %v", filepath, err)
-		os.Exit(1)
-	}
-
-	var jsonMap JsonDataType
-	if err := json.Unmarshal(jsonBytes, &jsonMap); err != nil {
-		log.Fatalf("Error fitting the json data into the jsonMap %v", err)
-		os.Exit(1)
-	}
-
-	// We are simulating `sets` using `maps`.
-	browsers := make(map[string]bool)
-	for _, browser := range jsonMap.Vars.Browsers {
-		browsers[browser] = true
-	}
-
-	terminals := make(map[string]bool)
-	for _, terminal := range jsonMap.Vars.Terminals {
-		terminals[terminal] = true
-	}
-
-	dirs := make(map[string]string)
-	for _, dirObj := range jsonMap.Vars.Dirs {
-		dirs[dirObj.Alias] = dirObj.Dir
-	}
-
-	aliases := make(AliasMap)
-	for _, aliasObj := range jsonMap.Aliases {
-		aliases[aliasObj.Alias] = CommandDataType{
-			Url:     aliasObj.Url,
-			Dir:     aliasObj.Dir,
-			AppData: aliasObj.AppData,
-		}
-	}
+	_, _, _, aliases := ExtractFromJson()
 
 	const urlAlias = "lc"
 
